f72d845b44f614676ed79aff71bcf2e5934c9cc8
realahnet <syedmusicrealise@gmail.com>
2026-02-26 02:58:22 +0500

    Spacewar: Ship dax config for dolby
    * Values adapted to sony's dolby stack.
    
    [Tashar02: Reformat XML using xmlstarlet]
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- device.mk
+++ device.mk
@@ -210,6 +210,9 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml
 
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/dax-default.xml:$(TARGET_COPY_OUT_VENDOR)/etc/dolby/dax-default.xml
+
 $(call soong_config_set_bool,android_hardware_audio,skip_speaker_layout_channel_mask_field,true)
 
 # Boot control
