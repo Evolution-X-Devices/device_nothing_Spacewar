ed82ead56c3a548bd40b0e7b6c39788b85f4bd17
BrainKub <fyhtjnt@gmail.com>
2026-04-28 12:30:37 +0300

    Reapply "Spacewar: Remove QTI perfd"
    This reverts commit aae038e48a7cfe60805d37663555258c50e38f55.
    
    Change-Id: Ia9ff539a1d01ff03dd50e1db21dd9419c73b360a

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -173,6 +173,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 VENDOR_SECURITY_PATCH := 2025-03-03
 
 # SELinux
+include device/lineage/sepolicy/libperfmgr/sepolicy.mk
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
